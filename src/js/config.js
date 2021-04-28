e8be83827e66781d2ea15555c178c5474d90325c
Emme243 <emmlop243@gmail.com>
2021-04-28 11:15:44 -0500

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/'
+export const TIMEOUT_SEC = 10
+export const RES_PER_PAGE = 10
+export const KEY = 'e02e01ba-ac4f-4c17-9111-0ac359a60b87'
+export const MODAL_CLOSE_SEC = 2.5
